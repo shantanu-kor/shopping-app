581afc9f43df9be308fa514d79bd47eb45b037e5
Shantanu Kor <77278554+shantanu-kor@users.noreply.github.com>
2024-02-25 13:30:28 +0530

    added dynamic routes.

--- app.js
+++ app.js
@@ -12,12 +12,10 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const contactusRoutes = require('./routes/contactus');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(contactusRoutes)
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
