a11ca0209b39c2acb91bb67120e777f5d587e901
Shantanu Kor <77278554+shantanu-kor@users.noreply.github.com>
2024-02-26 22:27:09 +0530

    Add files via upload

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: process.env.MYSQL_PASSWORD
+});
+
+module.exports = pool.promise();
